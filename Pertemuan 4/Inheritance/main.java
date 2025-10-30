c1132825009b3b038ec7a99519fec249bf4ac0ec
Raditya Naila Sahwa Fahrezzy <radityanaila21@gmail.com>
2025-10-30 16:01:41 +0700

    Add files via upload

---
+++ Pertemuan 4/Inheritance/main.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan_4_inheritance;
+
+/**
+ *
+ * @author radit
+ */
+public class main {
+    public static void main(String[] args) {
+        enemy monster = new enemy(); 
+        zombie jumbi = new zombie();
+        pocong cong = new pocong();
+        burung garuda = new burung();
+        
+        jumbi.attackPoint = 20;
+        jumbi.hp = 75;
+        jumbi.name = "Zombie";
+        System.out.println( jumbi.name + ", " + jumbi.hp + ", " + jumbi.attackPoint );
+        System.out.println("Action :");
+        jumbi.attack();
+        jumbi.walk();
+        System.out.println("--------------------X--------------------");
+        
+        cong.attackPoint = 45;
+        cong.hp = 40;
+        cong.name = "Cong";
+        System.out.println( cong.name + ", " + cong.hp + "," + cong.attackPoint );
+        System.out.println("Action :");
+        cong.attack();
+        cong.jump();
+        System.out.println("--------------------X--------------------");
+        
+        monster.attackPoint = 80;
+        monster.hp = 100;
+        monster.name = "Ciro";
+        System.out.println( monster.name + ", " + monster.hp + ", " + monster.attackPoint );
+        System.out.println("Action :");
+        monster.attack();
+        System.out.println("--------------------X--------------------");
+        
+        garuda.attackPoint = 70;
+        garuda.hp = 60;
+        garuda.name = "Garuda";
+        System.out.println( garuda.name + ", " + garuda.hp + ", " + garuda.attackPoint );
+        System.out.println("Action :");
+        garuda.fly();
+        garuda.jump();
+        garuda.walk();
+        garuda.attack();
+        System.out.println("--------------------X--------------------");
+    }
+}
---
+++ Pertemuan 4/Inheritance/zombie.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan_4_inheritance;
+
+/**
+ *
+ * @author radit
+ */
+public class zombie extends enemy {
+    void walk(){
+        System.out.println("Zombie Walking.");
+    }
+}
