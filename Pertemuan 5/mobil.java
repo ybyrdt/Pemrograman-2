e5db1adbc97906bb911f593faaebe787c48befcd
Raditya Naila Sahwa Fahrezzy <radityanaila21@gmail.com>
2026-01-21 10:08:12 +0700

    Add files via upload

---
+++ Pertemuan 5/mobil.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan_5;
+
+/**
+ *
+ * @author radit
+ */
+public class mobil {
+    public void jalankan(){
+        System.out.println("Masukkan gear 1, lepas kopling dan tekan gas.....");
+    }
+}
---
+++ Pertemuan 5/mobilMatic.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan_5;
+
+/**
+ *
+ * @author radit
+ */
+public class mobilMatic extends mobil {
+
+    @Override
+    public void jalankan(){
+        System.out.println("Gear diposisi D, tekan gas..");
+    }
+}
